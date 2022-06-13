7ed2d153ebc59dad382fa90fffd836646c425833
Lennon Fonseca <lennonfonsecaa@gmail.com>
2022-06-13 18:29:40 -0300

    adicionar calculos

---
+++ inicio.js
@@ -0,0 +1,51 @@
+let nome = "Lennon Fonseca";
+let idade = 27;
+let ano = 12;
+let dia = 30;
+let hora = 24;
+
+let resultado1 = (idade*ano);
+console.log ("resultado é:" + resultado1);
+
+let resultado2 = (resultado1*dia);
+console.log ("resultado é:" + resultado2);
+
+let resultado3 = (resultado2*hora);
+console.log ("resultado é:" + resultado3);
+
+let ano_atual = 2022;
+let ano_nascimento = 1995;
+
+let resultado4 = (ano_atual-ano_nascimento);
+console.log ("sua idade é:" + resultado4);
+
+let idade1 = 27;
+let ano1 = 12;
+let semana = 48;
+
+let resultado5 = (idade1*ano1);
+console.log ("resultado é:" + resultado5);
+
+let resultado6 = (resultado5*semana);
+console.log ("resultado é:" + resultado6);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
